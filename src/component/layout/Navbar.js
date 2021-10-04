bcf5b184f3a2322345ee17909ef17821e4af15d3
Sandeep <bishtsandy22@gmail.com>
2021-10-04 08:29:14 +0530

    Added basic Navbar and logo image

---
+++ src/component/layout/Navbar.js
@@ -0,0 +1,96 @@
+import React, { Fragment } from "react";
+import logo from "../../resources/images/desi-hip-hop.png";
+const Navbar = () => {
+  return (
+    <Fragment>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark bg-gradient">
+        <div className="container-fluid">
+          <a className="navbar-brand logo-image" href="#">
+            <img src={logo} className="img-fluid" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <ul className="navbar-nav ms-auto flex-nowrap mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Our Solution
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  How We Help
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Navbar;
