6bbffcf446004b57bc00701c5fffe27f55cbb161
Sandeep <bishtsandy22@gmail.com>
2021-10-12 10:09:53 +0530

    social media links with icon

--- src/component/common/SocialMedia.js
+++ src/component/common/SocialMedia.js
@@ -1,24 +1,30 @@
 import React, { Fragment } from "react";
 
 const SocialMedia = ({ socialaccount }) => {
-  const socialMediaMap = [
-    {
-      instagram: "fab fa-instagram",
-      facebook: "fab fa-facebook-square",
-      youtube: "fab fa-youtube",
-      twitter: "fab fa-twitter-square",
-    },
-  ]; //will move this thing in database later
+  const socialMediaMap = {
+    instagram: "fab fa-instagram-square fa-5x",
+    facebook: "fab fa-facebook-square fa-5x",
+    youtube: "fab fa-youtube-square fa-5x",
+    twitter: "fab fa-twitter-square fa-5x",
+  }; //will move this thing in database later
   const entries = Object.entries(socialaccount);
-  const key = entries[0][0];
-  const value = entries[0][1];
+  const socialMediaMapEntries = Object.entries(socialMediaMap);
+
+  const keyValue = entries[0][0];
+  let className = null;
+  Object.entries(socialMediaMap).map(([key, value]) => {
+    if (key === keyValue) {
+      className = value;
+    }
+  });
+  const clickLink = entries[0][1];
+
   return (
     <Fragment>
       {
-        <div>
-          <i className={socialMediaMap[key]} onClick={value}></i>
-          icons
-        </div>
+        <a href={clickLink} className="d-flex">
+          <i className={className} style={{ color: "black" }}></i>
+        </a>
       }
     </Fragment>
   );
