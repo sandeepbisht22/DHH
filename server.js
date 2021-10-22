f41dd4592ce8d0bbfab747c02a70b006d708a965
Sandeep <bishtsandy22@gmail.com>
2021-10-22 17:23:19 +0530

    added bsic get call and made express listen on port 5000

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-import Express from "express";
+import express from "express";
 import { connectMongoDB } from "./config/db.mjs";
+const app = express();
+
+// Making DB connection
 connectMongoDB();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.json({
+    msg: "DHH is here",
+  });
+});
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
