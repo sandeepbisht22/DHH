637caacecfe40e66985c718209c469fcaf12837e
Sandeep <bishtsandy22@gmail.com>
2021-10-26 13:40:52 +0530

    used Package sickle to add swipper in home page

--- client/src/component/pages/Home.js
+++ client/src/component/pages/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import HomeSlider from "../layout/HomeSlider";
 const Home = () => {
   return (
     <div>
-      {" "}
-      <div className="scroll">
+      <HomeSlider></HomeSlider>
+      {/* <div className="scroll">
         <div className="row flex-row flex-nowrap">
           <div className="col-3">
             <div className="card card-block">Card</div>
@@ -37,7 +37,7 @@ const Home = () => {
             <div className="card card-block">Card</div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
