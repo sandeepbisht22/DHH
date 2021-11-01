6df0f002d1e0f213253c7ce0d2a2df967b2a5800
Sandeep <bishtsandy22@gmail.com>
2021-11-01 20:22:07 +0530

    initial user page

--- client/src/component/pages/User.js
+++ client/src/component/pages/User.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import profileImage from "../../resources/images/defaultProfile.png";
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-9 border border-primary">user preference</div>
+        <div className="col-md-3 border border-primary">
+          <div className="row-cols-md-3 text-center">
+            <img
+              src={profileImage}
+              alt="Profile image"
+              style={{ width: "60%", height: "70%" }}
+              className="border rounded-circle"
+            />
+            <div className="p-2">Flex item 2</div>
+            <div className="p-2">Flex item 3</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default User;
